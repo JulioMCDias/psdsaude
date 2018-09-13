4574b677469b3b6ba461aebc20a5b826b2471028
zeemacpare <zeemacpare@uninove.edu.br>
2018-09-13 03:50:29 +0000

    Updated conexao.php

--- conexao.php
+++ conexao.php
@@ -1,15 +1,12 @@
 <?php 
-//Database=localdb;Data Source=127.0.0.1;User Id=azure;Password=6#vWHD_$;Port=56100
-$servername = "127.0.0.1:56100";
-$database = "psdsaude";
-$username = "azure";
-$password = "6#vWHD_$";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-mysqli_close($conn);
+     session_start();
+            global $pdo;  
+            try 
+            {
+              $pdo = new PDO("mysql:dbname=psdsaude;host=127.0.0.1;charset=utf8","azure", "6#vWHD_$");
+            } catch (PDOException $e) 
+            {
+              echo "FALHA NA CONEXÃO: ".$e->getMessage();
+              exit;
+            }
 ?>
