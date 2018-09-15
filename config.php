7cc00cb3bf9daaa47a20a1bc820ebb138d8b2804
zeemacpare <zeemacpare@uninove.edu.br>
2018-09-15 21:51:21 +0000

    Updated config.php

--- config.php
+++ config.php
@@ -4,7 +4,6 @@ session_start();
 	try 
 	{
 	  $pdo = new PDO("mysql:dbname=psdsaude;host=127.0.0.1:56100;charset=utf8","azure", "6#vWHD_$");
-	  echo "CONEXÃO BEM SUCEDIDA";
 	} catch (PDOException $e) 
 	{
 	  echo "FALHA NA CONEXÃO: ".$e->getMessage();
